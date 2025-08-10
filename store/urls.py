afd0e2e9956de96de8a7767fce0540358f326453
ajaysth <shrestha11ajay@gmail.com>
2025-08-10 09:23:52 +0545

    Revire nd rating system done

--- store/urls.py
+++ store/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('category/<slug:category_slug>/<slug:product_slug>', views.product_detail, name='products_detail'),
     path('search/', views.search, name='search'),
     path('autocomplete/', views.autocomplete, name='autocomplete'),
+    path('submit_review/<int:product_id>/', views.submit_review, name='submit_review')
 
 ]
